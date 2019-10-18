c166f1661bc02823f113c3d1f54926e38505eb64
hewel <hewelzei@qq.com>
2019-10-18 17:53:42 +0800

    fix: fix getPlaylist return value type

--- src/requests/getPlaylist.ts
+++ src/requests/getPlaylist.ts
@@ -1,6 +1,6 @@
 import axios from '../utils/axios'
 
-export default async function getPlaylist(id: string): Promise<Object> {
+export default async function getPlaylist(id: string): Promise<object> {
     const {
         data: { playlist },
     } = await axios.get('/playlist/detail', { params: { id } })
